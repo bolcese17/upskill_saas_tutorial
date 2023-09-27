a8c3ce3db86a78e52089e0a68f80488c6881a565
Brendan Olcese <brendanolcese@gmail.com>
2023-09-27 16:00:13 -0400

    still trying to fix mime magic

--- workspace/saasapp/config/application.rb
+++ workspace/saasapp/config/application.rb
@@ -11,8 +11,5 @@ module Saasapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    ENV['FREEDESKTOP_MIME_TYPES_PATH'] = Rails.root.join('config', 'freedesktop.org.xml').to_s
-
   end
 end
